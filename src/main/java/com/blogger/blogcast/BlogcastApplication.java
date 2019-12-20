32ec4a79ba5dea1a77bddcbab97676e742c3559d
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-20 18:40:59 -0500

    barebone spring app - running

---
+++ src/main/java/com/blogger/blogcast/BlogcastApplication.java
@@ -0,0 +1,13 @@
+package com.blogger.blogcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogcastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogger/blogcast/BlogcastApplicationTests.java
@@ -0,0 +1,16 @@
+package com.blogger.blogcast;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogcastApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
