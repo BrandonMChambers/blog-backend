12b8a2a59dddb26757eb33746284d395b4922d2d
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-21 19:42:08 -0500

    test

--- src/main/java/com/blogger/blogcast/BlogcastApplication.java
+++ src/main/java/com/blogger/blogcast/BlogcastApplication.java
@@ -1,13 +1,24 @@
 package com.blogger.blogcast;
 
+import com.blogger.blogcast.model.User;
+import com.blogger.blogcast.repository.UserRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BlogcastApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogcastApplication.class, args);
+	}
+
+	@Bean
+	ApplicationRunner applicationRunner(UserRepository userRepository){
+		return args -> {
+			userRepository.save(new User("Brandon"));
+		};
 	}
 
 }
