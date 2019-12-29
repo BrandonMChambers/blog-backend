d624d9e88ef40cef6239cb15677606abd20d0028
Brandon Chambers <brandonmchambers36@gmail.com>
2019-12-29 18:24:37 -0500

    Setup oauth-google

--- src/main/java/com/blogger/blogcast/security/SecurityConfiguration.java
+++ src/main/java/com/blogger/blogcast/security/SecurityConfiguration.java
@@ -1,21 +1,16 @@
 package com.blogger.blogcast.security;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
-@EnableOAuth2Sso
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeRequests()
+        http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
