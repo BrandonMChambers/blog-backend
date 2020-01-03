212891f12a97f649a5d4f96b404e49e4ef1fb569
wes <wes.jones1597@gmail.com>
2020-01-02 19:06:42 -0500

    When List<Blog> was changed to List<Long>, Spring errored due to mapping issues, so we need to fix this to make it compile

--- src/main/java/com/blogger/blogcast/model/User.java
+++ src/main/java/com/blogger/blogcast/model/User.java
@@ -17,11 +17,11 @@ public class User {
     @Size(min=2, max=30, message = "Username size should be in the range [2...30]")
     private String username;
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinColumn
     private List<Long> running; //Ids of Blogs
 
-    @OneToMany(cascade = CascadeType.ALL)
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinColumn
     private List<Long> following; //Ids of Blogs
 
